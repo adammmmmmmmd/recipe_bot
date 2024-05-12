97ab0171a6bb9e8de037eb50761dc08ab4a8c0f3
adammmmmmmmd <evg.omelchenko18@gmail.com>
2024-05-13 00:31:08 +0300

    config fix

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 TELEGRAM_TOKEN = '6745181155:AAGqoY2CvXa5L9JX1mW7U7XVrScei7J4T4A'
 
-MEAL_BASE_URL = 'www.themealdb.com/api/json/v1/1/search.php'
+MEAL_BASE_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
